24dd864db0088c06994075ece11bba1797f82833
pritikekan <pritikekan1998@gmail.com>
2023-05-08 09:10:20 +0530

    typeofAssignment
    typeofAssignment

---
+++ 03_typeOf.js
@@ -0,0 +1,26 @@
+var bankName;
+console.log("Name of bankName-->Type:",bankName);
+console.log("typeofbankName-->",bankName);
+bankName ="SBIBank";
+var typeOfbankName = typeof bankName;
+console.log( "Type of variable ->bankName: ", typeOfbankName);
+console.log("typeofbankName-->",bankName);
+console.log("--------------------------------------------------");
+var marks="90%";
+console.log("marks:",marks, "type:",typeof marks);
+console.log("--------------------------------------------------");
+var isWorking;
+console.log(isWorking);
+var isWorking = true;
+console.log("Type of IS Working:",isWorking);
+console.log("Type of variable:",typeof isWorking);
+console.log("--------------------------------------------------");
+var totalCount = "Hundred and Seven";
+console.log("totalCount:",totalCount);
+console.log("Type of variable:",typeof totalCount);
+
+ var totalCount = 107;
+ var typeOfTotalCount =typeof totalCount;
+console.log("totalCount:",totalCount);
+console.log("Type of variable:",typeof totalCount);
+
