e360078d01d83f8a6bc24ada901db7b9469d2c8f
pritikekan <pritikekan1998@gmail.com>
2023-05-08 09:07:47 +0530

    javascript basic
    javascript basic

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("priti kekan..");
+console.log("Address : Nashik");
+var firstName ="priti";
+console.log(firstName)
+
+var age;//varible declaration
+age="22";//varaiable Initalization
+console.log(age);
+
+var holder;
+console.log(holder);//undefined
+holder="My spec";//varaiable Initalization
+console.log(holder);
+
+holder ="Sachin Spec"; //variable update
+console.log(holder);
