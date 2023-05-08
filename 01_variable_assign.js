00a4191f45b79f397508cbc2fb9401b7ce7489ca
pritikekan <pritikekan1998@gmail.com>
2023-05-08 09:09:50 +0530

    variable Assignment
     variable Assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Priti kekan";
+console.log(myFullName);
+myFullName="Priti Chandrabhan Kekan";
+console.log(myFullName);
+
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+var firstName="Priti";
+console.log(firstName);
+var lastName="Kekan";
+console.log(lastName);
+var age="22";
+console.log(age);
+var address="Tal-sinnar Dist-Nashik";
+console.log(address);
+var pincode="422606";
+console.log(pincode);
+var gender="female";
+console.log(gender);
+var ismarried="NO"
+console.log(ismarried);
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,45 @@
+console.log("------ NUMBER-------");
+var mobileNo = 9657115670;
+var pin = 787;
+var student = 45;
+var typeOfmobileNo = typeof mobileNo;
+var typeOfpin = typeof pin;
+var typeOfstudent = typeof student;
+console.log( "Type of variable -> MobileNo: ", typeOfmobileNo);
+console.log("typeofmobileNo-->",mobileNo);
+console.log( "Type of variable ->pin: ", typeOfpin);
+console.log("typeofpin-->",pin);
+console.log( "Type of variable ->student: ", typeOfstudent);
+console.log("typeofstudent-->",student);
+
+console.log("------ Boolean-------");
+var isStudent = true;
+var sick = false; 
+var knowTesing = true;
+var typeOfisStudent = typeof isStudent;
+console.log( "Type of variable ->isStudent: ", typeOfisStudent);
+console.log("typeofisStudent-->",isStudent);
+var typeOfsick = typeof sick;
+console.log( "Type of variable ->sick: ", typeOfsick);
+console.log("typeofsick-->",sick);
+var typeOfknowTesing = typeof knowTesing;
+console.log( "Type of variable ->: knowTesing", typeOfknowTesing);
+console.log("typeofsick-->",knowTesing);
+
+console.log("------ string-------");
+var firstName = "priti";
+var Education = 'graduation'; 
+var branch = "IT";
+var typeOffirstName = typeof firstName;
+var typeOfEducation = typeof  Education;
+var typeOfbranch = typeof branch;
+console.log( "Type of variable ->: firstName", typeOffirstName);
+console.log("typeoffirstName-->",firstName);
+console.log( "Type of variable ->:Education", typeOfEducation);
+console.log("typeofEducation-->",Education);
+console.log( "Type of variable ->:branch", typeOfbranch);
+console.log("typeofbranch-->",branch);
+
+
+
+
